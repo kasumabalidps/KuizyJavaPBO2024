faebb9f06eb0053508a632b57a581aacaffec947
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-06-12 14:27:17 +0800

    update

---
+++ app/src/main/java/com/example/kuizyjava_pbo2024/PilihanQuizActivity.java
@@ -0,0 +1,30 @@
+package com.example.kuizyjava_pbo2024;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PilihanQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pilihan_quiz);
+
+        // Sembunyiin ActionBar di Atas
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
