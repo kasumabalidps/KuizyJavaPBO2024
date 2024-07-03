3b4c2d3ad3b23a72dddcd7090baab401547e453c
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-07-04 04:40:03 +0800

    update

--- app/src/main/java/com/example/kuizyjava_pbo2024/LoginActivity.java
+++ app/src/main/java/com/example/kuizyjava_pbo2024/LoginActivity.java
@@ -3,9 +3,12 @@ package com.example.kuizyjava_pbo2024;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -65,11 +68,24 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        // Sembunyiin ActionBar di Atas
+        // Enable edge-to-edge content
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
+        // Hide the action bar if present
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/kuizyjava_pbo2024/WelcomeActivity.java
+++ app/src/main/java/com/example/kuizyjava_pbo2024/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.kuizyjava_pbo2024;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -18,7 +22,21 @@ public class WelcomeActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
 
-        // Sembunyiin ActionBar di Atas
+        // Enable edge-to-edge content
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
+        // Hide the action bar if present
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
